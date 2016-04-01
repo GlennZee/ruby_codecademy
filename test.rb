4f4893f54593c4de3a625ce0e79804fb612b9086
Glenn Zieve <glennz@gmail.com>
2016-04-01 03:47:24 +0000

    new class files

--- test.rb
+++ test.rb
@@ -1,17 +1,13 @@
-crew = {
-  captain: "Picard",
-  first_officer: "Riker",
-  lt_cdr: "Data",
-  lt: "Worf",
-  ensign: "Ro",
-  counselor: "Troi",
-  chief_engineer: "LaForge",
-  doctor: "Crusher"
-}
-# Add your code below!
-
-first_half = lambda{ |key,value| value < "M" }
-
-a_to_m = crew.select(&first_half)
-
- a_to_m.each { |x,y| puts y }
+class Message
+   
+    @@messages_sent  = 0
+    
+    def initialize (from, to)
+        @from = from
+        @to = to
+        @@messages_sent += 1
+    end
+    end
+    
+    my_message = Message.new("Fred", "Mary")
+    puts my_message 
